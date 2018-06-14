bad24b75c9ef35ad065a447cd68ca66d27673278
lazurav <lazurav@gmail.com>
2018-06-14 20:55:37 +0300

    auth

--- resources/views/adminus/index.blade.php
+++ resources/views/adminus/index.blade.php
@@ -1,9 +1,10 @@
-@extends('layouts.app')
+@extends('layouts.adminus')
 
-@section('content')
+ @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+       
+        <div class="col-md-12">
             <div class="card">
                 <div class="card-header">Admin panel</div>
 
@@ -18,6 +19,13 @@
                 </div>
             </div>
         </div>
+
     </div>
 </div>
 @endsection
+
+@section('columnLeft')   
+    <h3>leftColumn</h3>
+@endsection
+
+
